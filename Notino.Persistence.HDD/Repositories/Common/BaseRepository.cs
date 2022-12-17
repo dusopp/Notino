89fd108c486d6f133f26b22056076109a005f24f
Juraj Glavas <glavasj7@gmail.com>
2022-12-17 15:49:13 +0100

    added comment Liskov Substistion Principle - not broken explanation

--- Notino.Persistence.HDD/Repositories/Common/BaseRepository.cs
+++ Notino.Persistence.HDD/Repositories/Common/BaseRepository.cs
@@ -8,10 +8,11 @@ using System.Threading.Tasks;
 namespace Notino.Persistence.HDD.Repositories.Common
 {
     /*
-     Methods that are not implemented and throw new NotImplementedException();
-     Are not implemented because lack of time.
-     They are necessary and they would be implemented.
-     I AM NOT BREAKING LISKOV SUBSTITUTION PRINCIPLE
+     Methods that are not implemented and throw new NotImplementedException(),
+     are not implemented because lack of time.
+     They are necessary and they would be implemented if I had enough time.
+     Therefore, I am thinking, that in current context: 
+      I AM NOT BREAKING LISKOV SUBSTITUTION PRINCIPLE
      */
 
     public class BaseRepository<T, TKey> : IRepository<T, TKey> 
